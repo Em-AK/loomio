d973a698fa116937b1c4b960180256e337fd657a
James Kiesel <james.kiesel@gmail.com>
2015-10-15 12:34:11 +0300

    Revert group controller spec

--- spec/controllers/api/groups_controller_spec.rb
+++ spec/controllers/api/groups_controller_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 describe API::GroupsController do
 
   let(:user) { create :user }
-  let(:another_user) { create :user }
-  let(:group) { create :group, is_visible_to_public: false }
+  let(:group) { create :group }
   let(:discussion) { create :discussion, group: group }
 
   before do
